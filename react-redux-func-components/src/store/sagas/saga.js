75f9f57b30943c1940eab47a9b2e3330340ee0cc
Illia Teliuk <illiateliuk@gmail.com>
2024-05-30 15:40:03 +0300

    Implemented saga functions

--- react-redux-func-components/src/store/sagas/saga.js
+++ react-redux-func-components/src/store/sagas/saga.js
@@ -1,3 +1,50 @@
-import { takeEvery, put } from "redux-saga/effects";
+import { takeEvery, put, all } from "redux-saga/effects";
+import { actionRequestType, actionSuccessType } from "../actions/actionTypes";
+import { v4 as uuidv4 } from "uuid";
 
-export function* rootSaga() {}
+function* workAddTodo({ payload }) {
+    payload.id = uuidv4();
+    yield put({ type: actionSuccessType.ADD_TODO_SUCCESS, payload: payload });
+}
+
+function* workDeleteTodo({ payload }) {
+    yield put({
+        type: actionSuccessType.DELETE_TODO_SUCCESS,
+        payload: payload,
+    });
+}
+
+function* workCheckTodo({ payload }) {
+    yield put({ type: actionSuccessType.CHECK_TODO_SUCCESS, payload: payload });
+}
+
+function* workEditTodo({ payload }) {
+    yield put({ type: actionSuccessType.EDIT_TODO_SUCCESS, payload: payload });
+}
+
+function* workClearCompleted() {
+    yield put({ type: actionSuccessType.CLEAR_COMPLETED_SUCCESS });
+}
+
+function* workSetFilter({ payload }) {
+    yield put({ type: actionSuccessType.SET_FILTER_SUCCESS, payload: payload });
+}
+
+function* todosSagas() {
+    yield takeEvery(actionRequestType.ADD_TODO_REQUEST, workAddTodo);
+    yield takeEvery(actionRequestType.DELETE_TODO_REQUEST, workDeleteTodo);
+    yield takeEvery(actionRequestType.CHECK_TODO_REQUEST, workCheckTodo);
+    yield takeEvery(actionRequestType.EDIT_TODO_REQUEST, workEditTodo);
+    yield takeEvery(
+        actionRequestType.CLEAR_COMPLETED_REQUEST,
+        workClearCompleted,
+    );
+}
+
+function* filtersSagas() {
+    yield takeEvery(actionRequestType.SET_FILTER_REQUEST, workSetFilter);
+}
+
+export default function* rootSaga() {
+    yield all([todosSagas(), filtersSagas()]);
+}
