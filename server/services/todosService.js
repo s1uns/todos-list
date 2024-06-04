5850f26c47299b3cdf496263100190544d6950ce
Illia Teliuk <illiateliuk@gmail.com>
2024-06-04 16:53:54 +0300

    todosService

--- server/services/todosService.js
+++ server/services/todosService.js
@@ -1,6 +1,30 @@
 const { makeRequest } = require("../db");
+const { v4 } = require("uuid");
 
+const addTodo = async (todo) => {
+    todo.id = v4;
 
-const addTodo = (todo) => {
+    await makeRequest(
+        `INSERT INTO todos (id, title, isCompleted, isUpdated, userId) VALUES (${todo.id}, ${todo.title}, FALSE, FALSE, userId); `,
+    );
+};
 
-}
+const deleteTodo = async (todoId) => {
+    await makeRequest(`DELETE FROM todos WHERE id = '${todoId}';`);
+};
+
+const updateTodo = async (newTodo) => {
+    await makeRequest(
+        `UPDATE todos SET title = ${newTodo.title}, isUpdated = TRUE WHERE id = '${todoId}';`,
+    );
+};
+
+const checkTodo = async (checkedTodo) => {
+    await makeRequest(
+        `UPDATE todos SET isCompleted = ${checkedTodo.isCompleted} WHERE id = '${checkedTodo.id}';`,
+    );
+};
+
+const clearCompleted = async () => {
+    await makeRequest(`DELETE FROM todos WHERE isCompleted = TRUE;`);
+};
