dd845032372da3652e64330cd22b25f91f0f0e16
Illia Teliuk <illiateliuk@gmail.com>
2024-06-06 21:55:51 +0300

    Added middleware to the app

--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@
 // const { todosRoutes } = require("./routes/todosRoutes/todosRoutes.js");
 import { router as authRouter } from "./routes/auth/index.js";
 import express from "express";
-
+import reqTimeMiddleware from "./middleware/reqTimeMiddleware.js";
+import responseMiddleware from "./middleware/responseMiddleware.js";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-const port =  process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT;
+
+app.use(reqTimeMiddleware, responseMiddleware);
 
 app.get("/", (req, res) => {
     res.send("Started Working, Express!");
