36dbf652f0246d7b7044781af1329d5a3d3f52fc
Illia Teliuk <illiateliuk@gmail.com>
2024-06-06 12:27:03 +0300

    Added expire time to jwt

--- server/services/auth/helpers.js
+++ server/services/auth/helpers.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 const secretKey = process.env.SECRET_KEY;
+const expiresIn = process.env.EXPIRES_IN
 
-const generateToken = async (user) => jwt.sign({ user }, secretKey);
+const generateToken = async (user) =>
+    jwt.sign({ user }, secretKey, { expiresIn: expiresIn });
 
 export { generateToken };
