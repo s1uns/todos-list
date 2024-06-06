65b605e787e06caf4650263c1e4ff52d80130b44
Illia Teliuk <illiateliuk@gmail.com>
2024-06-06 12:28:36 +0300

    Removed comments. destructurization

--- server/controllers/auth/register.js
+++ server/controllers/auth/register.js
@@ -1,23 +1,31 @@
 import { validateEmail, validatePassword } from "./helpers.js";
 import { registerUser } from "../../services/auth/index.js";
 
+
 const register = async (req, res) => {
-    console.log("Request: ", req.body);
-    const isEmailValid = await validateEmail(req.body.email);
+    const { email, firstName, lastName, username, password } = req.body;
+
+    const isEmailValid = await validateEmail(email);
 
     if (!isEmailValid) {
         res.status(422).send("The email you specified is wrong");
         return;
     }
 
-    const isPasswordValid = await validatePassword(req.body.password);
+    const isPasswordValid = await validatePassword(password);
 
     if (!isPasswordValid) {
         res.status(422).send("The password you specified is wrong");
         return;
     }
 
-    const token = await registerUser(req.body);
+    const token = await registerUser({
+        email,
+        firstName,
+        lastName,
+        username,
+        password,
+    });
     res.status(200).send(token);
 };
 
--- server/db.js
+++ server/db.js
@@ -3,7 +3,6 @@ import dbConfig from "./dbConfig.js";
 
 const connection = mysql.createPool(dbConfig);
 const makeRequest = async (query) => {
-    // console.log("Query: ", query);
 
     return new Promise((resolve, reject) => {
         connection.query(query, (err, rows, fields) => {
