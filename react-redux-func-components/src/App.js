409f6db465da97e32c6774f237ccadbcfa5f41ce
Illia Teliuk <illiateliuk@gmail.com>
2024-06-11 01:33:27 +0300

    useToast test

--- react-redux-func-components/src/App.js
+++ react-redux-func-components/src/App.js
@@ -4,11 +4,16 @@ import TodoListPage from "./pages/TodoListPage ";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import withAuth from "./components/withAuth";
+import { useToast } from "./components/toast/toast-context";
 
 function App() {
     const LoginWithAuth = withAuth(LoginPage, false);
     const RegisterWithAuth = withAuth(RegisterPage, false);
     const TodoListWithAuth = withAuth(TodoListPage, true);
+
+    const toast = useToast();
+
+    toast.open("This is toast message");
 
     return (
         <Routes>
