54168426db8fdeea1a3459b75ef9c312d7c828c0
Illia Teliuk <illiateliuk@gmail.com>
2024-06-17 15:52:59 +0300

    Create user update

--- server/models/user/createUser.js
+++ server/models/user/createUser.js
@@ -6,10 +6,23 @@ const createUser = async ({
     firstName,
     lastName,
     username,
+    birthDate,
+    gender,
+    country,
+    city,
+    heardFrom,
     password,
-}) =>
+}) => {
     await makeRequest(
-        `INSERT INTO users (id, email, firstName, lastName , username, password) VALUES ('${id}', '${email}', '${firstName}', '${lastName}', '${username}', '${password}');`,
+        `INSERT INTO users (id, email, username, firstName, lastName, birthDate, gender, country, city, password) VALUES ('${id}', '${email}', '${username}', '${firstName}', '${lastName}', '${birthDate}', '${gender}', '${country}', '${city}' '${password}');`,
     );
+
+    heardFrom.map(
+        async (heardFrom) =>
+            await makeRequest(
+                `INSERT INTO userheardfrom (id, value) VALUES ('${id}', ${heardFrom});`,
+            ),
+    );
+};
 
 export default createUser;
