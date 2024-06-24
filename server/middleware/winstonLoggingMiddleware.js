b2e55e4c0049bdb09e7370d240366e2ab68de2c5
Illia Teliuk <illiateliuk@gmail.com>
2024-06-24 09:51:09 +0300

    Updated date format

--- server/middleware/winstonLoggingMiddleware.js
+++ server/middleware/winstonLoggingMiddleware.js
@@ -1,11 +1,30 @@
 import { createLogger, format, transports } from "winston";
 import expressWinston, { requestWhitelist } from "express-winston";
 
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+  
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+  
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+  
+    const formattedDate = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+    
+    return formattedDate;
+  }
+  
+
+
 const logsFormat = format.printf((info) => {
     const { level, meta, message, timestamp } = info;
 
     if (meta) {
-        return `${timestamp} ${level}: ${meta.req.method} ${
+        return `${formatDate(timestamp)} ${level}: ${meta.req.method} ${
             meta.req.originalUrl
         } returned ${meta.res.statusCode} in ${meta.responseTime} ms ${
             meta.res.body?.message
@@ -14,7 +33,7 @@ const logsFormat = format.printf((info) => {
         }`;
     }
 
-    return `${timestamp} ${level}: ${message}`;
+    return `${formatDate(timestamp)} ${level}: ${message}`;
 });
 
 const loggerOptions = {
