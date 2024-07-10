d76229baaed6034dc827a8fb96ccf278848e65b5
Illia Teliuk <illiateliuk@gmail.com>
2024-07-10 17:20:03 +0300

    Find user by his username

--- server/controllers/user/getAvailableUsers.js
+++ server/controllers/user/getAvailableUsers.js
@@ -2,19 +2,20 @@ import { logger } from "../../middleware/winstonLoggingMiddleware.js";
 import { getAvailableUsers as getAvailableUsersAsync } from "../../services/user/index.js";
 
 const getAvailableUsers = async (req, res) => {
-    const userId = req.userId;
-    const { page, limit } = req.query;
+	const userId = req.userId;
+	const { page, limit, search } = req.query;
 
-    const result = await getAvailableUsersAsync({
-        page: +page ? +page : 1,
-        limit: +limit ? +limit : 3,
-        userId: userId,
-    });
+	const result = await getAvailableUsersAsync({
+		page: +page ? +page : 1,
+		limit: +limit ? +limit : 3,
+		search: search ? search : "",
+		userId: userId,
+	});
 
-    logger.info(
-        `User ${userId} returned a list of available users. Page: ${page}. Limit: ${limit}.`,
-    );
-    return res.success(result);
+	logger.info(
+		`User ${userId} returned a list of available users. Page: ${page}. Limit: ${limit}.`,
+	);
+	return res.success(result);
 };
 
 export default getAvailableUsers;
--- server/services/user/getAvailableUsers.js
+++ server/services/user/getAvailableUsers.js
@@ -2,41 +2,44 @@ import { Op } from "sequelize";
 import { Shared, Users } from "../../database/models/relations.js";
 import { SHARE_ACTIVE } from "../../utils/constants/sharedStatus.js";
 
-const getAvailableUsers = async ({ page, limit, userId }) => {
-    const queries = {
-        offset: (page - 1) * limit,
-        limit: limit,
-    };
+const getAvailableUsers = async ({ page, limit, userId, search }) => {
+	const queries = {
+		offset: (page - 1) * limit,
+		limit: limit,
+	};
 
-    const availableUsers = await Users.findAndCountAll({
-        where: {
-            id: {
-                [Op.not]: userId,
-            },
-        },
-        attributes: ["id", "username", "firstName", "lastName", "fullName"],
-        include: { model: Shared, as: "sharedWith", attributes: ["status"] },
-        ...queries,
-    });
+	const availableUsers = await Users.findAndCountAll({
+		where: {
+			id: {
+				[Op.not]: userId,
+			},
+			username: {
+				[Op.like]: `%${search}%`,
+			},
+		},
+		attributes: ["id", "username", "firstName", "lastName", "fullName"],
+		include: { model: Shared, as: "sharedWith", attributes: ["status"] },
+		...queries,
+	});
 
-    const list = availableUsers.rows.map((userInfo) => {
-        const shareStatus =
-            !!userInfo.sharedWith[0] &&
-            +userInfo.sharedWith[0].status === SHARE_ACTIVE
-                ? true
-                : false;
+	const list = availableUsers.rows.map((userInfo) => {
+		const shareStatus =
+			!!userInfo.sharedWith[0] &&
+			+userInfo.sharedWith[0].status === SHARE_ACTIVE
+				? true
+				: false;
 
-        return {
-            id: userInfo.id,
-            username: userInfo.username,
-            fullName: userInfo.fullName,
-            isShared: shareStatus,
-        };
-    });
+		return {
+			id: userInfo.id,
+			username: userInfo.username,
+			fullName: userInfo.fullName,
+			isShared: shareStatus,
+		};
+	});
 
-    const totalPages = Math.ceil(availableUsers?.count / limit);
+	const totalPages = Math.ceil(availableUsers?.count / limit);
 
-    return { list: list, totalPages: totalPages };
+	return { list: list, totalPages: totalPages };
 };
 
 export default getAvailableUsers;
